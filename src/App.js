81f29f236e72d3db9ef7167eeaca6a12ec88b058
rizarisyam <risyam23riza@gmail.com>
2022-02-09 11:44:17 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+      const data = await response.json();
+      console.log(data);
+      setPokemonData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p className="">{JSON.stringify(pokemonData.data)}</p>
     </div>
   );
 }
